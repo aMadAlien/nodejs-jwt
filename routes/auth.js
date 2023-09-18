0d92996793cff81e280de6e6f450169d4764eb6d
iren <irasibil2@gmail.com>
2023-09-18 12:07:35 +0300

    added validation

--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,16 @@
 const router = require('express').Router();
 const User = require('../models/User');
+const { registerValidation, loginValidation } = require('../validation');
 
 // /api/user/register
 router.post('/register', async (req, res) => {
+    // validation
+    const error = registerValidation(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+
+    // check if user exists
+    const emailExists = await User.findOne({ email: req.body.email });
+    if (emailExists) return res.status(400).send('Email already exists')
 
     // create a user
     const user = new User({
