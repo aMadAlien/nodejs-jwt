80fc91993396da3f392a7293616919ce15c871ac
iren <irasibil2@gmail.com>
2023-09-18 13:26:12 +0300

    added login functionality

--- routes/auth.js
+++ routes/auth.js
@@ -29,13 +29,24 @@ router.post('/register', async (req, res) => {
         res.status(200).send(savedUser);
     } catch (error) {
         res.status(400).send(error)
-        
     }
 })
 
 // /api/user/login
-router.post('/login', (req, res) => {
-    res.send('Login')
+router.post('/login', async (req, res) => {
+    // validation
+    const error = loginValidation(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+
+    // check if user exists
+    const user = await User.findOne({ email: req.body.email });
+    if (!user) return res.status(400).send('Email doesn\'t exist')
+
+    // validate password
+    const validPassword = await bcrypt.compare(req.body.password, user.password);
+    if (!validPassword) return res.status(400).send('Invalid password');
+
+    res.send('Logged in!')
 })
 
 module.exports = router;
